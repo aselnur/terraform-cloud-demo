7022c5cab3c9f00c8e13895dcc1fe4dd1e0b2865
aselnur <127999113+aselnur@users.noreply.github.com>
2023-06-20 19:44:09 -0500

    added private subnets

---
+++ rds/main.tf
@@ -0,0 +1,14 @@
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+
+  config = {
+    organization = "aselnur"
+    workspaces = {
+      name = "vpc"
+    }
+  }
+}
+resource "aws_db_subnet_group" "default" {
+  name       = "project-db-subnet-group"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
+}
